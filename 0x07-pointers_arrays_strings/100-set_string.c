fc7b8d585ee4d9270faf605bffbf9cbd429bb70e
root <kenmafabi@gmail.com>
2021-09-13 11:08:35 -0700

    a function that sets the value of a pointer to a char.

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,31 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_diagsums - print sums of diagonals in matrix
- * @a: matrix
- * @size: size of matrix
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer to a char
+ * @to: the address to match
+ *
+ * Return: void
  */
-
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-
-	int diagonal_sum_1 = 0;
-	int diagonal_sum_2 = 0;
-	int row, i;
-
-	for (row = 0; row < size; row++)
-	{
-		i = (row * size) + row;
-		diagonal_sum_1 += a[i];
-	}
-
-	for (row = 1; row <= size; row++)
-	{
-		i = (row * size) - row;
-		diagonal_sum_2 += a[i];
-	}
-
-	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
-
+	*s = to;
 }
