a9ef313fdc5a98018c8b8b4bd0f2201aa69e9b97
root <kenmafabi@gmail.com>
2021-09-23 09:36:40 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,56 @@
-#include "holberton.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
  */
-int _strlen(char *s)
-{
-	int i = 0;
 
-	while (*s++)
-		i++;
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
 	return (i);
 }
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: copy first n bytes of s2
- *
- * Return: void
+ * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s, *r;
-	unsigned int l1, l2;
+	char *ptr;
+	int num, len, i, j;
 
-	if (!s1)
+	num = n;
+
+	if (s1 == NULL) /* account for NULL strings */
 		s1 = "";
-	if (!s2)
+	if (s2 == NULL)
 		s2 = "";
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-
-	l2 = l2 > n ? n : l2;
-	r = s = malloc(l1 + l2 + 1);
-	if (!s)
+	if (num < 0) /* account for negative n bytes */
 		return (NULL);
-	while (*s1)
-		*s++ = *s1++;
-	while (l2--)
-		*s++ = *s2++;
-	*s = '\0';
-	return (r);
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
+
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+
+	return (ptr);
 }
