b396ed875832dc5c86fc7b4697ef4553a37007d3
Mafabi Kennedy <63707180+kenoict@users.noreply.github.com>
2022-06-24 14:06:26 +0300

    Function that adds a new node at the end of a linked list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,34 +1,39 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
- * add_dnodeint_end - adds new node to end of dlist
- * @head: address of pointer to current head node
- * @n: int field of new node
- *
- * Return: address of new node or NULL
+ * add_dnodeint_end - Adds a new node at the end of a dlist_t list.
+ * @head: Head pointer
+ * @n: Data
+ * Return: list.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t)), *node;
+	dlistint_t *new_node, *node;
 
-	if (!head || !new)
-		return (new ? free(new), NULL : NULL);
+	if (head == NULL)
+		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
-	if (!*head)
-	{
-		new->prev = NULL;
-		*head = new;
+	new_node = malloc(sizeof(list_t)); /* 1. allocate node */
+	if (new_node == NULL)
+		return (NULL);
+
+	node = *head;
+	new_node->n = n; /* put in the data  */
+	new_node->next = NULL; /* New node is last node, next of it as NULL*/
+
+	if (!*head) /* If Linked List is empty, new node as head */
+	{	new_node->prev = NULL;
+		*head = new_node;
+		return (*head);
 	}
-	else
-	{
-		node = *head;
-		while (node->next)
-			node = node->next;
+	while (node->next) /* Traverse till the last node */
+		node = node->next;
 
-		node->next = new;
-		new->prev = node;
-	}
-	return (new);
+	node->next = new_node; /* Change the next of last node */
+	new_node->prev = node; /* Prev of new node to last node */
+	return (new_node);
 }
